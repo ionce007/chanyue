bdc2a96837d1e6102225159230eed8e929c2f49b
明空 <867528315@qq.com>
2023-08-15 22:16:00 +0800

    [优化功能] 新增多语言支持，优化部分功能

--- server/app/middleware/init.js
+++ server/app/middleware/init.js
@@ -8,29 +8,37 @@ const FriendlinkService = require("../service/api/friendlink");
 
 module.exports = () => {
   return async (req, res, next) => {
-    if ("site" in res.locals) {
-      console.log("已存在缓存，不需要重新在取");
-      await next();
-      return;
-    }
-    // 站点
-    const site = await SiteService.find();
-    site.json = JSON.parse(site.json);
-    // 分类
-    const category = await CommonService.category();
-    //导航
-    const nav = tree(category);
-    // 友情链接
-    let friendlink = await FriendlinkService.list();
-    friendlink = friendlink.list;
-    //样式路径
-    const base_url = `/public/template/${template}`;
-    //获取碎片 默认获取100条
-    const frag = await FragService.list();
-    //获取tag标签 默认100条
-    const tag = await TagService.list();
 
-    res.locals = { site, nav, category, friendlink, base_url, frag, tag };
-    await next();
+    try {
+      if ("site" in res.locals) {
+        console.log("已存在缓存，不需要重新在取");
+        await next();
+        return;
+      }
+      // 站点
+      const site = await SiteService.find();
+      site.json = site.json ? JSON.parse(site.json) : {};
+      // 分类
+      const category = await CommonService.category();
+      //导航
+      const nav = tree(category);
+      // 友情链接
+      let friendlink = await FriendlinkService.list();
+      friendlink = friendlink.list || [];
+      //样式路径
+      const base_url = `/public/template/${template}`;
+      //获取碎片 默认获取100条
+      const frag = await FragService.list();
+      //获取tag标签 默认100条
+      const tag = await TagService.list();
+
+      res.locals = { site, nav, category, friendlink, base_url, frag, tag };
+      await next();
+    } catch (error) {
+      next(error)
+    }
+
   };
+
+
 };
