5ce0204d3dc427608cafdcf00116a9ad22147534
明空 <867528315@qq.com>
2023-10-13 13:24:35 +0800

    [新增功能] 系统配置优化

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const run = require("./app/middleware/run.js");
 const app = express();
 //运行
 run(app);
+
 app.listen(port, () => {
   console.log(
     ` ${appName} 启动成功！\r\n localhost:${port} 版本:${version}`
--- server/app/middleware/config.js
+++ server/app/middleware/config.js
@@ -2,6 +2,7 @@ const SysAppService = require('../modules/api/service/sysApp.js');
 const config = require("../config/config.js");
 const auth = require("./auth.js");
 const helper = require("../extend/helper.js");
+
 // 所有配置入口
 module.exports = async function(app){
     let sysconfig = await SysAppService.find();
@@ -9,4 +10,3 @@ module.exports = async function(app){
     app.locals.auth = auth;
     app.locals.helper = helper;
 }
-
